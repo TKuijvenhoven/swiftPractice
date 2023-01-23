e6b3c2d1cded105acacf83469001ed81e9869cbd
TKuijvenhoven <94169966+TKuijvenhoven@users.noreply.github.com>
2023-01-23 14:15:14 +0100

    Add files via upload

---
+++ B-Naming and Identifiers/Naming and Identifiers.playground/Pages/09-ExerciseShow.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+let numberOfTickets = 150
+
+let ticketPrice = 10
+
+let roomRentalFee = 1000
+
+let posterCost = 40
+
+// Caculation
+let totalValue = numberOfTickets * ticketPrice
+
+let totalExpenses = roomRentalFee + posterCost
+
+let totalIncome = totalValue - totalExpenses
---
+++ B-Naming and Identifiers/Naming and Identifiers.playground/Pages/Naming Things.xcplaygroundpage/Contents.swift
@@ -0,0 +1,31 @@
+let numberOfDogs = 6 + 2
+
+let numberOfCats = 5
+
+let numberOfTurtles = 2
+
+let numberOfHamsters = 1
+//: After you’ve defined a constant and assigned a value to it, you can use the constant wherever you would have used the value, including in mathematical calculations that define the value of yet another constant:
+ 
+let totalNumberOfAnimals = numberOfDogs + numberOfCats + numberOfTurtles + numberOfHamsters
+
+let totalNumberOfMammals = numberOfDogs + numberOfCats + numberOfHamsters
+
+/*:
+Now it will be much easier — and less error-prone — to update the number of a certain kind of animal. You only have to update the value where the constant is defined.
+
+You can even let the playground do the math for you by adding or subtracting on the right-hand side of the equal sign. For example, if two more people were bringing a dog, instead of changing the 6 to an 8, you could type:
+
+`let numberOfDogs = 6 + 2`
+
+ - Experiment: As the week goes on, you get even more information about people’s pets.\
+Update the code above for the following updates:
+   - Two more people are bringing a dog.
+   - One of the cats cannot make it.
+   - The sick turtle is feeling much better and will be coming to the pet show.
+   - Another person is bringing a hamster.
+
+Once you’re finished experimenting, move ahead.
+
+[Previous](@previous)  |  page 4 of 14  |  [Next: Typing Names and Autocompletion](@next)
+ */
